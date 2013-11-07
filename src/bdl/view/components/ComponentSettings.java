61318711c0a2ebe8fa47bb4b4a3c3c9851f1a74e
Ben Goodwin <bg93@kent.ac.uk>
2013-11-07 15:57:34 +0000

    Update methods

--- src/bdl/view/components/ComponentSettings.java
+++ src/bdl/view/components/ComponentSettings.java
@@ -3,42 +3,39 @@ package bdl.view.components;
 import java.util.Collection;
 
 /**
- * Component Settings stores editable values for components.
- * Stores the settings and properties, layout and listener options for each 
- * component that will be used by the property editing panes to list the options
- * available to edit for each component. Will be initialised after the
- * associated xml file has been read by ComponenttViewReader.
- * 
+ * Component Settings stores editable values for components. Stores the settings
+ * and properties, layout and listener options for each component that will be
+ * used by the property editing panes to list the options available to edit for
+ * each component. Will be initialised after the associated xml file has been
+ * read by ComponenttViewReader.
+ *
  * @author Ben Goodwin
  */
 class ComponentSettings {
-    
-    private static String name;
-    private static Collection<Properties> properties;
-    private static Collection<Layout> layout;
-    private static Collection<Listeners> listeners;
 
-    
-    public static Collection<Properties> getProperties() {
+    private String name;
+    private Collection<Properties> properties;
+    private Collection<Layout> layout;
+    private Collection<Listeners> listeners;
+
+    public Collection<Properties> getProperties() {
         return properties;
     }
-    
-    public static Collection<Layout> getLayout() {
+
+    public Collection<Layout> getLayout() {
         return layout;
     }
-    
-    public static Collection<Listeners> getListeners() {
+
+    public Collection<Listeners> getListeners() {
         return listeners;
     }
-    
-    public static String getName() {
+
+    public String getName() {
         return name;
     }
-    
-    
-    
+
     private static class Properties {
-        
+
         public Properties() {
         }
     }
--- src/bdl/view/components/ComponentViewReader.java
+++ src/bdl/view/components/ComponentViewReader.java
@@ -1,5 +1,7 @@
 package bdl.view.components;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -7,25 +9,37 @@ import java.util.Collection;
  * @author Ben Goodwin
  */
 class ComponentViewReader {
- 
-    private static Collection<Collection<ComponentSettings>> xml = ParseSettings();
-    
-    public static Collection<ComponentSettings> getSettingsByName(String name) {
-     
-        return null;
-    }
-    
-    public static Collection<Collection<ComponentSettings>> allComponentsWithProperties() {
-        
+
+    private static Collection<ComponentSettings> allComponents = ParseSettings();
+
+    public static ComponentSettings getSettingsByName(String name) {
+        for (ComponentSettings cs : allComponents) {
+            if (cs.getName().equals(name)) {
+                return cs;
+            }
+        }
         return null;
     }
 
-    private static Collection<Collection<ComponentSettings>> ParseSettings() {
-        
+    public static Collection<ComponentSettings> allComponents() {
+        return allComponents;
+    }
+
+    public static Collection<ComponentSettings> allComponentNames() {
+        ArrayList al = new ArrayList<>();
+        for (ComponentSettings cs : allComponents) {
+            al.add(cs.getName());
+        }
+        return allComponents;
+    }
+
+    private static Collection<ComponentSettings> ParseSettings() {
+        try {
+            File settings = new File("component-options.xml");
+        } catch (Exception e) {
+            System.out.println("Cannot find file.");
+        }
+
         return null;
     }
-    
-    
-    
-    
 }
