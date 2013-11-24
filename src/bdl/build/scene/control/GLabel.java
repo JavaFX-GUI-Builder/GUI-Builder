2f2f532e168d635b2143dd5265c1fc21b75aacd9
Leon Atherton <la217@kent.ac.uk>
2013-11-24 21:55:12 +0000

    Case 9 - Removed unnecessary comment

--- src/bdl/build/scene/control/GLabel.java
+++ src/bdl/build/scene/control/GLabel.java
@@ -4,13 +4,6 @@ import bdl.build.GObject;
 import bdl.build.GType;
 import javafx.scene.control.Label;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Leon
- * Date: 24/11/13
- * Time: 20:58
- * To change this template use File | Settings | File Templates.
- */
 public class GLabel extends Label implements GObject {
     private String fieldName;
 
