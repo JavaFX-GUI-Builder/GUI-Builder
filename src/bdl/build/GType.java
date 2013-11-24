1f088256ba7f5f1554a91053e4cc7bfafb78cc36
Leon Atherton <la217@kent.ac.uk>
2013-11-24 20:13:44 +0000

    Case 12 - Added import values for types

--- src/bdl/build/GType.java
+++ src/bdl/build/GType.java
@@ -1,14 +1,24 @@
 package bdl.build;
 
 public enum GType {
-    Button,
-    Menu,
-    MenuBar,
-    MenuItem,
-    ToolBar,
-    Image,
-    ImageView,
-    AnchorPane;
+    Button("javafx.scene.control.Button"),
+    Menu("javafx.scene.control.Menu"),
+    MenuBar("javafx.scene.control.MenuBar"),
+    MenuItem("javafx.scene.control.MenuItem"),
+    ToolBar("javafx.scene.control.ToolBar"),
+    Image("javafx.scene.image.Image"),
+    ImageView("javafx.scene.image.ImageView"),
+    AnchorPane("javafx.scene.layout.AnchorPane");
+
+    private String importStatement;
+
+    GType (String importStatement) {
+        this.importStatement = "import " + importStatement + ";";
+    }
+
+    public String getImportStatement() {
+        return importStatement;
+    }
 
     @Override
     public String toString() {
