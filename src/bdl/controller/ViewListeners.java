dbe3065eabe03033b164031887c063bc1edfaaeb
Ben Goodwin <bg93@kent.ac.uk>
2014-01-17 16:07:30 +0000

    Selection box now draws correctly for Circles.

--- src/bdl/controller/ViewListeners.java
+++ src/bdl/controller/ViewListeners.java
@@ -5,12 +5,13 @@ import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 public class ViewListeners {
+
     private Rectangle outline;
     private View view;
-
     boolean isMousePressed = false;
 
     public ViewListeners(View view) {
@@ -28,27 +29,46 @@ public class ViewListeners {
         isMousePressed = true;
         curX = mouseEvent.getX();
         curY = mouseEvent.getY();
-
-        double nodeX = node.getLayoutX();
-        double nodeY = node.getLayoutY();
-        Bounds bounds = node.getLayoutBounds();
-        double nodeW = bounds.getWidth();
-        double nodeH = bounds.getHeight();
-        outline.setLayoutX(nodeX - 4);
-        outline.setLayoutY(nodeY - 4);
-        outline.setWidth(nodeW + 8);
-        outline.setHeight(nodeH + 8);
+        if (node instanceof Circle) {
+            double nodeX = node.getLayoutX();
+            double nodeY = node.getLayoutY();
+            Bounds bounds = node.getLayoutBounds();
+            double nodeW = bounds.getWidth();
+            double nodeH = bounds.getHeight();
+            outline.setLayoutX(nodeX - 4 - (nodeW / 2));
+            outline.setLayoutY(nodeY - 4 - (nodeH / 2));
+            outline.setWidth(nodeW + 8);
+            outline.setHeight(nodeH + 8);
+        } else {
+            double nodeX = node.getLayoutX();
+            double nodeY = node.getLayoutY();
+            Bounds bounds = node.getLayoutBounds();
+            double nodeW = bounds.getWidth();
+            double nodeH = bounds.getHeight();
+            outline.setLayoutX(nodeX - 4);
+            outline.setLayoutY(nodeY - 4);
+            outline.setWidth(nodeW + 8);
+            outline.setHeight(nodeH + 8);
+        }
     }
-
     double curX = 0, curY = 0;
+
     public void onMouseDragged(Node node, MouseEvent mouseEvent) {
         if (isMousePressed) {
             double x = node.getLayoutX() + (mouseEvent.getX() - curX);
             double y = node.getLayoutY() + (mouseEvent.getY() - curY);
             node.setLayoutX(x);
             node.setLayoutY(y);
-            outline.setLayoutX(x - 4);
-            outline.setLayoutY(y - 4);
+            if (node instanceof Circle) {
+                Bounds bounds = node.getLayoutBounds();
+                double nodeW = bounds.getWidth();
+                double nodeH = bounds.getHeight();
+                outline.setLayoutX(x - 4 - (nodeW / 2));
+                outline.setLayoutY(y - 4 - (nodeH / 2));
+            } else {
+                outline.setLayoutX(x - 4);
+                outline.setLayoutY(y - 4);
+            }
         }
     }
 
