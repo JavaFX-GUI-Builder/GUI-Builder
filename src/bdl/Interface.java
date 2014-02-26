d56fa2f0693305c6c4595d2d5241cd64b547c1ae
David Hodgson <dh296@kent.ac.uk>
2014-02-26 06:44:39 +0000

    Minor organisation.

--- src/bdl/Interface.java
+++ src/bdl/Interface.java
@@ -1,11 +1,5 @@
 package bdl;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Platform;
 import javax.swing.SwingUtilities;
 
@@ -17,29 +11,35 @@ import javax.swing.SwingUtilities;
  */
 public class Interface {
     
+    // If you are not working with the BlueJ project, comment out the following section of BlueJ code.
+    public static bluej.pkgmgr.PkgMgrFrame pmf;
+    public static void SetPkgMgrFrame(bluej.pkgmgr.PkgMgrFrame pmf) { Interface.pmf = pmf; }
+    // Interface from GUI Builder -> BlueJ
+    public static void TestWriteMessage(final String msg) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // TODO: Attempt to communicate back to BlueJ.
+                pmf.setStatus(msg);
+            }
+        });
+    }
+    
     // Private fields
     private static javafx.stage.Stage stage;
     private static bdl.controller.Controller controller;
     private static boolean started = false;
     private static ClassLoader cl;
-    private static bluej.pkgmgr.PkgMgrFrame pmf;
-    //private static Class<?> interfaceToBlueJ;
     
-    // Public methods
+    // Interface from BlueJ -> GUI Builder
     public static void Show() {
         if (started == false) { Start(); }
         else { stage.show(); }
     }
     public static void Hide() { stage.hide(); }
-    public static void SetUsingBlueJ(boolean bool) {
-        // TODO -- Determines whether BlueJ-specific features are enabled,
-        // or whether the application should run standalone
-        // (e.g. standalone = don't send code updates to BlueJ)
-    }
     public static void SetStage(javafx.stage.Stage stage) { Interface.stage = stage; }
     public static void SetController(bdl.controller.Controller controller) { Interface.controller = controller; }
     public static void SetClassLoader(ClassLoader cl) { Interface.cl = cl; }
-    public static void SetPkgMgrFrame(bluej.pkgmgr.PkgMgrFrame pmf) { Interface.pmf = pmf; }
     public static void TestToggleHistory() {
         Platform.runLater(new Runnable() { 
             @Override
@@ -56,22 +56,6 @@ public class Interface {
                 }
             }
         });
-    }
-//    public static void BlueJSetup() {
-//        try {
-//            interfaceToBlueJ = Class.forName("bdl.InterfaceToBlueJ", true, cl);
-//        } catch (ClassNotFoundException ex) {
-//            Logger.getLogger(Interface.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
-    public static void TestWriteMessage(final String msg) {
-        SwingUtilities.invokeLater(new Runnable() {
-        @Override
-        public void run() {
-            // TODO: Attempt to communicate back to BlueJ.
-            pmf.setStatus(msg);
-        }
-    });
     }
     
     // Private methods
